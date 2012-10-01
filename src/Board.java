a651138bcff01bd9e936a9882de373ff8be51a61
Sondre Lucas Follesø <sondre@dogu.no>
2012-10-01 10:34:29 +0200

    MAJOR REFACTOR!

--- src/Board.java
+++ src/Board.java
@@ -1,38 +1,170 @@
 import java.util.ArrayList;
 
+import com.sun.corba.se.impl.interceptors.PICurrent;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 
 public class Board {
-	private ArrayList<ArrayList<Piece>> board;
+	
+	private ArrayList<Piece> board;
+	private ArrayList<Piece> pieces;
 	
 
 	public Board(){
-		this.board = new ArrayList<ArrayList<Piece>>();
-		for(int i=0; i<4; i++){
-			ArrayList<Piece> emptyRow = new ArrayList<Piece>();
-			emptyRow.add(null);
-			emptyRow.add(null);
-			emptyRow.add(null);
-			emptyRow.add(null);
-			board.add(emptyRow);
+		this.board = new ArrayList<Piece>();
+		for(int i=0; i<16; i++){
+			board.add(null);
 		}
+		this.pieces = new ArrayList<Piece>();
+		for(Piece.Color c: Piece.Color.values()){
+			for(Piece.Height h: Piece.Height.values()){
+				for(Piece.Shape s: Piece.Shape.values()){
+					for(Piece.Consistensy con: Piece.Consistensy.values()){
+						pieces.add(new Piece(c, h, s, con));
+					}
+				}
+			}
+		}
+	}
+	
+	public ArrayList<Piece> getPieces() {
+		return pieces;
+	}
+
+	public void setPieces(ArrayList<Piece> pieces) {
+		this.pieces = pieces;
+	}
+
+	public boolean checkForWinner(){
+		
+		for (int i = 0; i < 4; i++) {
+			ArrayList<Piece> row = this.getRow(i);
+			Piece first = row.remove(0);
+			
+			boolean color = true;
+			boolean height = true;
+			boolean shape = true;
+			boolean consistensy = true;
+			
+			for (Piece piece : row) {
+				if(!first.getColor().equals(piece.getColor())){
+					color = false;
+				}
+				if(!first.getHeight().equals(piece.getHeight())){
+					height = false;
+				}
+				if(!first.getShape().equals(piece.getShape())){
+					shape = false;
+				}
+				if(!first.getConsistensy().equals(piece.getConsistensy())){
+					consistensy = false;
+				}
+			}			
+			if(color || height || shape || consistensy){
+				return true;
+			}
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			ArrayList<Piece> col = this.getColumn(i);
+			Piece first = col.remove(0);
+			boolean color = true;
+			boolean height = true;
+			boolean shape = true;
+			boolean consistensy = true;
+			
+			for (Piece piece : col) {
+				if(!first.getColor().equals(piece.getColor())){
+					color = false;
+				}
+				if(!first.getHeight().equals(piece.getHeight())){
+					height = false;
+				}
+				if(!first.getShape().equals(piece.getShape())){
+					shape = false;
+				}
+				if(!first.getConsistensy().equals(piece.getConsistensy())){
+					consistensy = false;
+				}
+			}
+			
+			if(color || height || shape || consistensy){
+				return true;
+			}
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			if(i==0){
+				ArrayList<Piece> diaR = this.getDiagonalRight();
+				Piece first = diaR.remove(0);
+				boolean color = true;
+				boolean height = true;
+				boolean shape = true;
+				boolean consistensy = true;
+				
+				for (Piece piece : diaR) {
+					if(!first.getColor().equals(piece.getColor())){
+						color = false;
+					}
+					if(!first.getHeight().equals(piece.getHeight())){
+						height = false;
+					}
+					if(!first.getShape().equals(piece.getShape())){
+						shape = false;
+					}
+					if(!first.getConsistensy().equals(piece.getConsistensy())){
+						consistensy = false;
+					}
+				}
+				
+				if(color || height || shape || consistensy){
+					return true;
+				}
+			}
+			else{
+				ArrayList<Piece> diaL = this.getDiagonalLeft();
+				Piece first = diaL.remove(0);
+				boolean color = true;
+				boolean height = true;
+				boolean shape = true;
+				boolean consistensy = true;
+				
+				for (Piece piece : diaL) {
+					if(!first.getColor().equals(piece.getColor())){
+						color = false;
+					}
+					if(!first.getHeight().equals(piece.getHeight())){
+						height = false;
+					}
+					if(!first.getShape().equals(piece.getShape())){
+						shape = false;
+					}
+					if(!first.getConsistensy().equals(piece.getConsistensy())){
+						consistensy = false;
+					}
+				}
+				
+				if(color || height || shape || consistensy){
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 	
 	public void clearBoard(){
 		board.clear();
-		for(int i=0; i<4; i++){
-			ArrayList<Piece> emptyRow = new ArrayList<Piece>();
-			emptyRow.add(null);
-			emptyRow.add(null);
-			emptyRow.add(null);
-			emptyRow.add(null);
-			board.add(emptyRow);
+		for(int i=0; i<16; i++){
+			board.add(null);
 		}
 	}
 	
-	public void placePiece(int x, int y, Piece piece){
+	public void placePiece(int index, Piece piece){
 		if(!this.board.isEmpty()){
-			if(this.board.get(x).get(y) == null){
-				this.board.get(x).set(y, piece);
+			if(this.board.get(index) == null){
+				this.board.add(index, piece);
+				this.pieces.remove(piece);
 			}
 			else{
 				System.out.println("This place is allready taken!");
@@ -40,10 +172,10 @@ public class Board {
 		}
 	}
 	
-	public Piece getPiece(int x, int y){
+	public Piece getPiece(int index){
 		if(!this.board.isEmpty()){
-			if(this.board.get(x).get(y) != null){
-				return this.board.get(x).get(y);
+			if(this.board.get(index)!= null){
+				return this.board.get(index);
 			}
 			else{
 				return null;
@@ -54,25 +186,59 @@ public class Board {
 		}
 	}
 
-	public ArrayList<ArrayList<Piece>> getBoard() {
+	public ArrayList<Integer> freePlaces(){
+		ArrayList<Integer> free = new ArrayList<Integer>();
+		for (int i = 0; i < board.size(); i++) {
+			if(board.get(i)==null){
+				free.add(i);
+			}
+		}
+		return free;
+	}
+
+	public ArrayList<Piece> getBoard() {
 		return board;
 	}
 
-	public void setBoard(ArrayList<ArrayList<Piece>> board) {
+	public void setBoard(ArrayList<Piece> board) {
 		this.board = board;
 	}
 	
-	public ArrayList<Piece> getRow(int index){
-		return this.board.get(index);
+	public ArrayList<Piece> getRow(int rowNr){
+		ArrayList<Piece> row = new ArrayList<Piece>();
+		for (int i = rowNr*4; i < (rowNr*4)+4; i++) {
+			row.add(this.board.get(i));
+		}
+		return row;
 	}
 	
-	public ArrayList<Piece> getColumn(int index){
-		ArrayList<Piece> col = new ArrayList<>();
-		for (int i = 0; i < 4; i++) {
-			col.add(board.get(i).get(index));
+	public ArrayList<Piece> getColumn(int colNr){
+		ArrayList<Piece> col = new ArrayList<Piece>();
+		for (int i = colNr; i < colNr*4; i+=4) {
+			col.add(board.get(colNr));
 		}
 		return col;
 	}
+	
+	public ArrayList<Piece> getDiagonalRight(){
+		ArrayList<Piece> dia = new ArrayList<>();
+		for (int i = 0; i < 16; i+=5) {
+			dia.add(board.get(i));
+		}
+		return dia;
+	}
+	public ArrayList<Piece> getDiagonalLeft(){
+		ArrayList<Piece> dia = new ArrayList<>(); 
+		for (int i = 3; i <= 12; i+=3) {
+			dia.add(board.get(i));
+		}
+		return dia;
+	}
+	
+	public ArrayList<Piece> remainingPieces(){
+		return pieces;
+	}
+	
 	
 	
 }
--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,12 @@
 
-import java.util.ArrayList;
 
 
 public class Game {
 	
 	private Board board;
-	private ArrayList<Piece> pieces;
 	
 	public Game(){
 		this.board = new Board();
-		this.pieces = new ArrayList<Piece>();
-		for(Piece.Color c: Piece.Color.values()){
-			for(Piece.Height h: Piece.Height.values()){
-				for(Piece.Shape s: Piece.Shape.values()){
-					for(Piece.Consistensy con: Piece.Consistensy.values()){
-						pieces.add(new Piece(c, h, s, con));
-					}
-				}
-			}
-		}
 	}
 	
 	public Board getBoard() {
@@ -27,75 +15,6 @@ public class Game {
 
 	public void setBoard(Board board) {
 		this.board = board;
-	}
-
-	public ArrayList<Piece> getPieces() {
-		return pieces;
-	}
-
-	public void setPieces(ArrayList<Piece> pieces) {
-		this.pieces = pieces;
-	}
-
-	public boolean checkForWinner(){
-		
-		for (int i = 0; i < 4; i++) {
-			ArrayList<Piece> row = board.getRow(i);
-			Piece first = row.get(0);
-			boolean color = true;
-			boolean height = true;
-			boolean shape = true;
-			boolean consistensy = true;
-			
-			for(int j = 1; j < 4; j++){
-				if(!first.getColor().equals(row.get(j).getColor())){
-					color = false;
-				}
-				if(!first.getHeight().equals(row.get(j).getHeight())){
-					height = false;
-				}
-				if(!first.getShape().equals(row.get(j).getShape())){
-					shape = false;
-				}
-				if(!first.getConsistensy().equals(row.get(j).getConsistensy())){
-					consistensy = false;
-				}
-			}
-			
-			if(color || height || shape || consistensy){
-				return true;
-			}
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			ArrayList<Piece> col = board.getColumn(i);
-			Piece first = col.get(0);
-			boolean color = true;
-			boolean height = true;
-			boolean shape = true;
-			boolean consistensy = true;
-			
-			for(int j = 1; j < 4; j++){
-				if(!first.getColor().equals(col.get(j).getColor())){
-					color = false;
-				}
-				if(!first.getHeight().equals(col.get(j).getHeight())){
-					height = false;
-				}
-				if(!first.getShape().equals(col.get(j).getShape())){
-					shape = false;
-				}
-				if(!first.getConsistensy().equals(col.get(j).getConsistensy())){
-					consistensy = false;
-				}
-			}
-			
-			if(color || height || shape || consistensy){
-				return true;
-			}
-		}
-		
-		return false;
 	}
 
 
