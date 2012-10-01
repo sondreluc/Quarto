6f29661b5dd89524ea0158f943fb10af1dd0500c
Øystein Heimark <oystein@heimark.no>
2012-10-01 11:07:31 +0200

    Set up basic game

--- src/QuartoGame/Game.java
+++ src/QuartoGame/Game.java
@@ -7,11 +7,13 @@ public class Game {
 	private Board board;
 	private Player player1;
 	private Player player2;
+	private Player activePlayer;
 	
 	public Game(Player p1, Player p2){
 		this.board = new Board();
 		this.player1 = p1;
 		this.player2 = p2;
+		this.activePlayer = p1;
 	}
 	
 	public Board getBoard() {
@@ -21,6 +23,28 @@ public class Game {
 	public void setBoard(Board board) {
 		this.board = board;
 	}
+	
+	public void playTurn(){
+		Piece piece = this.activePlayer.pickPiece(board);
+		this.activePlayer.placePiece(board, piece);
+		
+		if(this.activePlayer == this.player1)
+			this.activePlayer = this.player2;
+		else
+			this.activePlayer = this.player1;
+			
+	}
+	
+	public void printBoard(){
+		System.out.println(board.printBoard());
+		
+	}
+	
+	public boolean isFinished(){
+		
+		return board.checkForWinner();
+		
+	}
 
 	
 	
@@ -28,7 +52,23 @@ public class Game {
 		
 		Game newGame = new Game(new Player(PlayerType.RANDOM), new Player(PlayerType.RANDOM));
 		
+		boolean finished = false;
+				
+		while(!finished){
+			newGame.playTurn();
+			
+			newGame.printBoard();
+			
+			if(newGame.isFinished()){
+				finished = true;
+				break;
+			}
+			else if(newGame.getBoard().getPieces().size() == 0)
+				finished = true;
+					
+			
+		}
+		
 	}
 
-	
 }
